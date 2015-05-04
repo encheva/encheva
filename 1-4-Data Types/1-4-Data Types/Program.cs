0293a35488ef3f96e065475244fc1cae9815bc3d
encheva <dessi_encheva@yahoo.com>
2015-05-04 18:54:00 +0300

    Data Types

---
+++ 1-4-Data Types/1-4-Data Types/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_4_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            int z = 5;
+            Console.WriteLine(x+z);
+        }
+    }
+}
---
+++ 5-4-Data Types/5-4-Data Types/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_4_Data_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string b = "He is";
+            string c = " a boy";
+            Console.WriteLine();
+            Console.WriteLine(b+c);
+            Console.WriteLine();
+        }
+    }
+}

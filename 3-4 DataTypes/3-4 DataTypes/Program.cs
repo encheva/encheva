18afd76f37d82a46a6e8fdc25e34bb2f60099d6a
encheva <dessi_encheva@yahoo.com>
2015-05-09 12:38:30 +0300

    Data Types

---
+++ 3-4 DataTypes/3-4 DataTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_4_DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведете стойността на страната на триъгълника");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Въведете стойността на височината на триъгълника");
+            int h = int.Parse(Console.ReadLine());
+
+            int s = (a * h) / 2;
+
+            Console.WriteLine("Лицето на вашия триъгълник е:" );
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ 4-4 Data Tipes/4-4 Data Tipes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_4_Data_Tipes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number...");
+
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter a number...");
+
+            int b = int.Parse(Console.ReadLine());
+
+            int c = a % b;
+
+            Console.WriteLine("Остатъкът от вашето деление е:");
+
+            Console.WriteLine(c);
+        }
+    }
+}
